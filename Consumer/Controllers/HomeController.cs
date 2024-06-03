05b250950c33f81337689d98f1156e1a58c743c0
ASUS <ASUS@DESKTOP-LD37JSJ>
2024-06-03 05:22:09 +0400

    Add project files.

---
+++ Consumer/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Consumer.Models;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Diagnostics;
+using System.Text;
+
+namespace Consumer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string selectedOption = "orange")
+        {
+            var messages = ReceiveMessages(selectedOption);
+            ViewBag.Messages = messages;
+            return View();
+        }
+
+        private List<string> ReceiveMessages(string selectedOption)
+        {
+            var messages = new List<string>();
+
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqps://psqyqkas:QYmaSp0TjmkU91ADQ-m0oc8AZFob-vhj@puffin.rmq2.cloudamqp.com/psqyqkas")
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_exchange", type: ExchangeType.Topic);
+                var queueName = channel.QueueDeclare().QueueName;
+
+                channel.QueueBind(queue: queueName, exchange: "topic_exchange", routingKey: selectedOption);
+
+                var data = channel.BasicGet(queueName, true);
+                if (data != null)
+                {
+                    var message = Encoding.UTF8.GetString(data.Body.ToArray());
+                    messages.Add(message);
+                }
+
+            }
+
+            return messages;
+        }
+    }
+}
---
+++ Publisher/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Publisher.Models;
+using RabbitMQ.Client;
+using System.Diagnostics;
+using System.Text;
+
+namespace Publisher.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string selectedOption = "orange", string message = "")
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqps://psqyqkas:QYmaSp0TjmkU91ADQ-m0oc8AZFob-vhj@puffin.rmq2.cloudamqp.com/psqyqkas")
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_exchange", type: ExchangeType.Topic);
+
+                var routingKey = selectedOption;
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "topic_exchange", routingKey: routingKey, basicProperties: null, body: body);
+            }
+
+            return View();
+        }
+    }
+}
